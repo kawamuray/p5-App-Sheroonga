404b75133835b3515511c663b124340ced9ca827
Yuto KAWAMURA(kawamuray) <kawamuray.dadada@gmail.com>
2013-10-31 20:23:31 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Sheroonga - It's new $module
+
+# SYNOPSIS
+
+    use App::Sheroonga;
+
+# DESCRIPTION
+
+App::Sheroonga is ...
+
+# LICENSE
+
+Copyright (C) Yuto KAWAMURA(kawamuray).
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Yuto KAWAMURA(kawamuray) <kawamuray.dadada@gmail.com>
---
+++ lib/App/Sheroonga.pm
@@ -0,0 +1,39 @@
+package App::Sheroonga;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Sheroonga - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Sheroonga;
+
+=head1 DESCRIPTION
+
+App::Sheroonga is ...
+
+=head1 LICENSE
+
+Copyright (C) Yuto KAWAMURA(kawamuray).
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yuto KAWAMURA(kawamuray) E<lt>kawamuray.dadada@gmail.comE<gt>
+
+=cut
+
